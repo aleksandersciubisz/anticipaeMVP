dd44c1bb6a3a8c0da425a2853453aa2d3e292d20
aleksandersciubisz <aleksander.sciubisz@gmail.com>
2021-10-26 21:48:41 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,61 @@ sr.reveal(`.sr-down-fast`, { delay: 200, origin: 'bottom' })
 sr.reveal(`.sr-down-slow`, { delay: 400, origin: 'bottom' })
 sr.reveal(`.sr-top-fast`, { delay: 200, origin: 'top' })
 sr.reveal(`.sr-top-slow`, { delay: 400, origin: 'top' })
+
+// console main page element
+consoleText(
+  [
+    'streamline data collection',
+    'find patterns, make decisions, move faster',
+    'gain an advantage and win your future',
+  ],
+  'text',
+  ['var(--white-color)', 'var(--white-color)', 'var(--white-color)']
+)
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff']
+  var visible = true
+  var con = document.getElementById('console')
+  var letterCount = 1
+  var x = 1
+  var waiting = false
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function () {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function () {
+        var usedColor = colors.shift()
+        colors.push(usedColor)
+        var usedWord = words.shift()
+        words.push(usedWord)
+        x = 1
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x
+        waiting = false
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true
+      window.setTimeout(function () {
+        x = -1
+        letterCount += x
+        waiting = false
+      }, 5000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x
+    }
+  }, 80)
+  window.setInterval(function () {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true
+    }
+  }, 400)
+}
